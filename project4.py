1d8b2914ad5c71826ea728e3f9eba32a57be63eb
nshenkman <nikita.shenkman@gmail.com>
2016-11-11 15:50:41 -0500

    working and fast

--- project4.py
+++ project4.py
@@ -1,38 +1,20 @@
 import errno
-import select
 import socket
 import re
 import time
 import sys
 from HTMLParser import HTMLParser
-sys.setrecursionlimit(10000000)
 
 USER_HOMEPAGE_REGEX = re.compile('/fakebook/\d+')
-
-USERNAME = '001947001'
-PASSWORD = 'UGLJ1MLC'
+USERNAME = sys.argv[1]
+PASSWORD = sys.argv[2]
+BUFFER = 10
 
 cookie = {}
-
-SOCKET = None
-
-searched_friends = [] #/fakebook/131231231, /fakebook/32131231/friends/1
-
+searched_friends = []  # /fakebook/131231231, /fakebook/32131231/friends/1
 flags = []
-
-counter = 0
-
-
-
-def setup_socket():
-    print 'starting server'
-    global SOCKET
-    SOCKET = socket.socket()
-    SOCKET.connect(('fring.ccs.neu.edu', 80))
-    SOCKET.setblocking(0)
-
-
-setup_socket()
+sockets = {}
+to_send = []
 
 
 def parse_friend(attributes):
@@ -40,7 +22,9 @@ def parse_friend(attributes):
         tag = attribute[0]
         value = attribute[1]
         if tag == 'href' and USER_HOMEPAGE_REGEX.match(value) and value not in searched_friends:
-            GET(value)
+            def get(): GET(value)
+
+            to_send.append(get)
 
 
 # create a subclass and override the handler methods
@@ -51,15 +35,13 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if 'FLAG' in data:
-            if data not in flags:
-                print data
-                flags.append(data)
-            if len(flags) == 5:
-                print 'DONE'
-                print flags
-                sys.exit(0)
+            flag = data.split('FLAG: ')[1]
 
-parser = MyHTMLParser()
+            if flag not in flags:
+                flags.append(flag)
+            if len(flags) == 5:
+                print("\n".join(flags))
+                sys.exit(0)
 
 
 def handle_moved_response(response_array):
@@ -72,27 +54,20 @@ def handle_moved_response(response_array):
 
 
 def handle_response(path, response):
-    global cookie, SOCKET, counter
+    global cookie
     response_array = response.rstrip().split('\n')
     move_request = False
     for response_line in response_array:
         if 'HTTP/1.1 200 OK' in response_line:
             searched_friends.append(path)
-            counter += 1
-            #print 'completed requests ' + str(counter)
-            print 'searched friends ' + str(len(searched_friends))
-            print "OK"
+            print len(searched_friends)
         elif 'HTTP/1.1 302 FOUND' in response_line or 'HTTP/1.1 301 MOVED PERMANENTLY' in response_line:
-            print "MOVED"
             move_request = True
         elif 'HTTP/1.1 500 INTERNAL SERVER ERROR' in response_line:
-            print "INTERNAL SERVER ERROR"
-            setup_socket()
             GET(path)
         elif 'Set-Cookie: ' in response_line:
             cookie_string = response_line.split('Set-Cookie: ')[1].split(';')[0]
             cookie_array = cookie_string.split('=')
-            print 'updating cookie %s to %s' % (cookie_array[0], cookie_array[1])
             cookie[cookie_array[0]] = cookie_array[1]
     if move_request:
         handle_moved_response(response_array)
@@ -101,40 +76,34 @@ def handle_response(path, response):
 
 
 def GET(path):
-    #print 'GET ' + path
     global cookie
+
+    s = socket.socket()
+    s.connect(('fring.ccs.neu.edu', 80))
+    s.setblocking(0)
+
     cookie_string = ''
     for name, value in cookie.items():
         cookie_string += name + '=' + value + '; '
 
     if cookie_string != '':
         cookie_string = cookie_string[:-2]
-    request = 'GET '+path+' HTTP/1.1 \nHost: fring.ccs.neu.edu\nAccept: text/html,application/xhtml+xml,application/xml\nAccept-Language: en-US'
+    request = 'GET ' + path + ' HTTP/1.1 \nHost: fring.ccs.neu.edu\nAccept: text/html,application/xhtml+xml,application/xml\nAccept-Language: en-US'
     if cookie_string:
         request += '\nCookie: ' + cookie_string
     CRLF = "\r\n\r\n"
     request += CRLF
-    while True:
-        try:
-            SOCKET.send(request)
-            response = SOCKET.recv(4096)
-            if len(response) == 0:
-                setup_socket()
-                GET(path)
-            else:
-                handle_response(path, response)
-            break
-        except socket.error, e:
-            err = e.args[0]
-            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-                time.sleep(.1)
-            else:
-                print e
-                sys.exit(1)
+    s.send(request)
+    sockets[path] = s
 
 
 def POST(path, form):
     global cookie
+
+    s = socket.socket()
+    s.connect(('fring.ccs.neu.edu', 80))
+    s.setblocking(0)
+
     cookie_string = ''
     for name, value in cookie.items():
         cookie_string += name + '=' + value + ';'
@@ -148,70 +117,56 @@ def POST(path, form):
 
     request_header = \
         "POST " + path + " HTTP/1.1\r\n" \
-        "Host: {hostname}\r\n" \
-        "Connection: keep-alive\r\n" \
-        "Content-Type: application/x-www-form-urlencoded\r\n" \
-        "Content-Length: {len}\r\n" \
-        "Cookie: {cookies}\r\n\r\n"\
+                         "Host: {hostname}\r\n" \
+                         "Connection: keep-alive\r\n" \
+                         "Content-Type: application/x-www-form-urlencoded\r\n" \
+                         "Content-Length: {len}\r\n" \
+                         "Cookie: {cookies}\r\n\r\n" \
             .format(hostname='fring.ccs.neu.edu', len=len(form_string), cookies=cookie_string)
 
     request = request_header + form_string
-    while True:
-        try:
-            SOCKET.send(request)
-            response = SOCKET.recv(4096)
-            handle_response(path, response)
-            break
-        except socket.error, e:
-            err = e.args[0]
-            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-                time.sleep(.1)
-            else:
-                print e
-                sys.exit(1)
+    s.send(request)
+    sockets[path] = s
+
 
 
 def search_friends(response):
     parser.feed(response)
 
-
-def login():
-    GET('/accounts/login/?next=/fakebook/\r')
-    csrftoken = cookie['csrftoken']
-    POST('/accounts/login/', {"username": USERNAME, "password": PASSWORD, "csrfmiddlewaretoken": csrftoken,
-                             "next": "%2Ffakebook%2F"})
-    GET('/fakebook/')
-
-login()
+GET('/accounts/login/?next=/fakebook/\r')
+parser = MyHTMLParser()
 
 
-# while True:
-#     try:
-#         response = SOCKET.recv(4096)
-#     except socket.error, e:
-#         err = e.args[0]
-#         if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-#             time.sleep(.1)
-#             print 'Waiting...'
-#             continue
-#         else:
-#             print e
-#             print err
-#             sys.exit(1)
-#     else:
-#         if len(response) == 0:
-#             GET(path)
-#         elif "sessionid" not in cookie:
-#             handle_response(response)
-#             csrftoken = cookie['csrftoken']
-#             POST('/accounts/login/', {"username": USERNAME, "password": PASSWORD, "csrfmiddlewaretoken": csrftoken,
-#                                       "next": "%2Ffakebook%2F"})
-#         else:
-#             # for path in in_flight:
-#             #     if path in response:
-#             #         in_flight.remove(path)
-#             #         searched_friends.append(path)
-#             #     else:
-#             #         GET(path)
-#             handle_response(response)
+while True:
+    if len(sockets) < BUFFER and len(to_send) > BUFFER - len(sockets):
+        for i in range(0, BUFFER - len(sockets)):
+            get = to_send[i]
+            get()
+            del to_send[i]
+    elif len(to_send) <= BUFFER - len(sockets):
+        for get in to_send:
+            get()
 
+    for path, s in sockets.items():
+        try:
+            response = s.recv(4096)
+        except socket.error, e:
+            err = e.args[0]
+            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                time.sleep(.1)
+                continue
+            else:
+                print e
+                sys.exit(1)
+        else:
+            s.close()
+            del sockets[path]
+            if len(response) == 0:
+                GET(path)
+            elif "sessionid" not in cookie:
+                handle_response(path, response)
+                csrftoken = cookie['csrftoken']
+                POST('/accounts/login/', {"username": USERNAME, "password": PASSWORD, "csrfmiddlewaretoken": csrftoken,
+                                          "next": "%2Ffakebook%2F"})
+            else:
+                handle_response(path, response)
